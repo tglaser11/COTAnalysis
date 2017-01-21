b771d13866fdfcb330fe1196671525737ce655c6
tglaser11 <tfglaser@gmail.com>
2017-01-21 11:31:48 -0600

    Initial commit

---
+++ COTAnalysis.py
@@ -0,0 +1,38 @@
+'''
+Gather Futures Commitment of Traders data and perform sentiment analysis using a methodology
+similar to what Larry Williams uses in his book: Trade Stocks and Commodities with the Insiders.
+
+Explore Gold, Silver and Copper
+
+'''
+import pandas as pd
+import Quandl as quandl
+
+# Gather Data
+goldCOT_df = quandl.get('CFTC/GC_FO_ALL')
+goldprice_df = quandl.get('CHRIS/CME_GC1', start_date="2006-06-01")
+
+
+# Drop unwanted fields
+goldprice_df = goldprice_df.drop(['Open','High','Low','Change','Settle','Volume','Previous Day Open Interest'], axis=1)
+
+# Combine datasets
+goldCOT_df = pd.concat([goldCOT_df, goldprice_df], axis=1, join_axes=[goldCOT_df.index])
+del goldprice_df
+
+# Fix missing price data by inerpolating between data points using timeseries
+goldCOT_df['Last'] = goldCOT_df['Last'].interpolate(method='time')
+
+
+'''
+Create a number of new features:
+(1) Open Interest Index
+(2) Commercial Position Index
+(3) Small Trader Position Index
+(4) Commerical / OI Index
+(5) Trends: Price Up & OI Up (True / False)
+(6) Trends: Price Up & OI Down (True / False)
+(7) Trends: Price Down & OI Up (True / False)
+(8) Trends: Price Down & OI Down (True / False)
+
+'''
