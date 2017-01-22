7e1cf7859687d32937779f1d646ac8bff420c8f6
tglaser11 <tfglaser@gmail.com>
2017-01-22 10:31:31 -0600

    Started OI Index work

--- COTAnalysis-gold.py
+++ COTAnalysis-gold.py
@@ -12,6 +12,19 @@ import Quandl as quandl
 goldCOT_df = quandl.get('CFTC/GC_FO_ALL')
 goldprice_df = quandl.get('CHRIS/CME_GC1', start_date="2006-06-01")
 
+'''
+goldCOT_df columns:
+
+Index([u'Open Interest', u'Producer/Merchant/Processor/User Longs',
+       u'Producer/Merchant/Processor/User Shorts', u'Swap Dealer Longs',
+       u'Swap Dealer Shorts', u'Swap Dealer Spreads', u'Money Manager Longs',
+       u'Money Manager Shorts', u'Money Manager Spreads',
+       u'Other Reportable Longs', u'Other Reportable Shorts',
+       u'Other Reportable Spreads', u'Total Reportable Longs',
+       u'Total Reportable Shorts', u'Non Reportable Longs',
+       u'Non Reportable Shorts'],
+      dtype='object')
+'''
 
 # Drop unwanted fields
 goldprice_df = goldprice_df.drop(['Open','High','Low','Change','Settle','Volume','Open Interest'], axis=1)
@@ -37,27 +50,77 @@ Create a number of new feature types:
 
 '''
 
+# Lookback Windows
+lookback1 = 26
+lookback2 = 52
+
 '''
 Open Interest Index
 Normalized form of OI to determine if there is enthusiasm in the market.
 High value may indicate over enthusiasm
 
-OI Index = ((OI(recent) - OI(min)) / (OI(max) - OI(min))) * 100
-
-
+OI Index = ((OI(recent) - OI(rollingmin)) / (OI(rollingmax) - OI(rollingmin))) * 100
 '''
 
-# Open Interest Lookback Windows
-OIlookback1 = 26
-OILookback2 = 52
-
-lb_window = goldCOT_df['Open Interest'].rolling(window = OIlookback1)
+lb_window = goldCOT_df['Open Interest'].rolling(window = lookback1)
 goldCOT_df['OIIndex1'] = ((goldCOT_df['Open Interest'] - lb_window.min()) / (lb_window.max() - lb_window.min())) * 100
 
-lb_window = goldCOT_df['Open Interest'].rolling(window = OIlookback2)
+lb_window = goldCOT_df['Open Interest'].rolling(window = lookback2)
 goldCOT_df['OIIndex2'] = ((goldCOT_df['Open Interest'] - lb_window.min()) / (lb_window.max() - lb_window.min())) * 100
 
 
+'''
+Commercial Position Index
+Normalized form of Commercial Position to determine what 'pros' are doing
 
+CP = net commercial position
+CP Index = ((CP(recent) - CP(rollingmin)) / (CP(rollingmax) - CP(rollingmin)) * 100
+'''
+
+goldCOT_df["netCP"] = goldCOT_df["Producer/Merchant/Processor/User Longs"] - goldCOT_df["Producer/Merchant/Processor/User Shorts"]
+
+lb_window = goldCOT_df['netCP'].rolling(window = lookback1)
+goldCOT_df['CPIndex1'] = ((goldCOT_df['netCP'] - lb_window.min()) / (lb_window.max() - lb_window.min())) * 100
+
+lb_window = goldCOT_df['netCP'].rolling(window = lookback2)
+goldCOT_df['CPIndex2'] = ((goldCOT_df['netCP'] - lb_window.min()) / (lb_window.max() - lb_window.min())) * 100
+
+
+'''
+Small Trader Position Index
+Normalized form of Small Trader Position to determine what 'joe public' is doing
+
+ST = net small trader position
+ST Index = ((ST(recent) - ST(rollingmin)) / (ST(rollingmax) - ST(rollingmin)) * 100
+'''
+
+goldCOT_df["netST"] = goldCOT_df["Non Reportable Longs"] - goldCOT_df["Non Reportable Shorts"]
+
+lb_window = goldCOT_df['netST'].rolling(window = lookback1)
+goldCOT_df['STIndex1'] = ((goldCOT_df['netST'] - lb_window.min()) / (lb_window.max() - lb_window.min())) * 100
+
+lb_window = goldCOT_df['netST'].rolling(window = lookback2)
+goldCOT_df['STIndex2'] = ((goldCOT_df['netST'] - lb_window.min()) / (lb_window.max() - lb_window.min())) * 100
+
+
+
+'''
+Commercial Position as a Percentage of OI Index
+Normalized form of Commercial Position relative to OI to determine what 'pros' are doing
+
+
+CP% = CP Index / OI
+CP% Index = ((CP%(recent) - CP%(rollingmin)) / (CP%(rollingmax) - CP%(rollingmin)) * 100
+'''
+goldCOT_df["CPPercent"] = goldCOT_df["netCP"] / goldCOT_df["Open Interest"]
+
+lb_window = goldCOT_df['CPPercent'].rolling(window = lookback1)
+goldCOT_df['CPPercentIndex1'] = ((goldCOT_df['CPPercent'] - lb_window.min()) / (lb_window.max() - lb_window.min())) * 100
+
+lb_window = goldCOT_df['netST'].rolling(window = lookback2)
+goldCOT_df['CPPercentIndex2'] = ((goldCOT_df['CPPercent'] - lb_window.min()) / (lb_window.max() - lb_window.min())) * 100
+
+
+goldCOT_df.tail()
 
 
